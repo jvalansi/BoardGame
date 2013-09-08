8aeff51789cd205f55a2cdb67ec57b49fcd8014f
jvalansi <jvalansi@gmail.com>
2013-09-08 16:55:20 +0300

    content from GridView

---
+++ BoardGame/src/com/example/boardgame/Game.java
@@ -0,0 +1,15 @@
+package com.example.boardgame;
+/**
+ * 
+ * @author jordan
+ *
+ *	Tic Tac Toe Game
+ *	Board game
+ *	turn based
+ *	on each turn player selects tile to place a unit
+ *	game ends when a whole line is covered by player's units
+ */
+public class Game {
+	
+	
+}
--- BoardGame/src/com/example/boardgame/MainActivity.java
+++ BoardGame/src/com/example/boardgame/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.boardgame;
 
+import com.example.boardgame.ImageAdapter;
+import com.example.boardgame.MainActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
+	    GridView gridview = (GridView) findViewById(R.id.gridview);
+	    //    GridView gridview = new GridView(getApplicationContext());
+	    gridview.setAdapter(new ImageAdapter(this));
+	
+	    gridview.setOnItemClickListener(new OnItemClickListener() {
+	    	public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	    		Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+	    	}
+	    });
+	}	
+	    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
